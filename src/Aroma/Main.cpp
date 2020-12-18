e3d3cf85ba7daa0d4d5a827b091742e31f2df322
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2020-12-18 16:25:25 -0500

    Add application QSS stylesheet.

--- src/Aroma/Main.cpp
+++ src/Aroma/Main.cpp
@@ -1,10 +1,19 @@
 #include <Aroma/MainWindow.hpp>
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleFile(":/styles/App.qss");
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString stylesheet = styleFile.readAll();
+        styleFile.close();
+        a.setStyleSheet(stylesheet);
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
